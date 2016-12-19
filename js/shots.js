7c81647351588ac1af3001e300ddb0eb15a377c1
lukegelmi <luke@runparis.fr>
2016-12-19 21:20:11 +0100

    added more js to masonry to make load after images have loaded, tidied everything else up a bit

--- js/shots.js
+++ js/shots.js
@@ -1,7 +1,7 @@
-$(window).ready(function() {
-	$('.grid').masonry({
-	  itemSelector: '.grid-item',
-	  columnWidth: '.grid-sizer',
-	  percentPosition: true
-	});
+$('.grid').imagesLoaded( function(){
+  $('.grid').masonry({
+   	itemSelector: '.grid-item',
+	columnWidth: '.grid-sizer',
+	percentPosition: true
+  });
 });
